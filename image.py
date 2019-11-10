f4bc41a49b1e4a85017687bcbe16fbba83ce5e27
ALambot <antoine.lambot@student.uclouvain.be>
2019-11-10 14:54:32 +0100

    clean image.py

--- image.py
+++ image.py
@@ -1,23 +1,37 @@
 import numpy as np
 import tifffile as tif
 from PIL import Image
+import imageio
 
 image_num = "1180"
 
-image = tif.imread("Paris_"+image_num+"/RGB-PanSharpen_AOI_3_Paris_img"+image_num+".tif")
+# Loading a tif file
+#image = tif.imread("Paris_"+image_num+"/RGB-PanSharpen_AOI_3_Paris_img"+image_num+".tif")
+
+# Loading a png file
+image = imageio.imread("Paris_"+image_num+"/after.png")
+
 sh = image.shape
 print(sh)
 
+# R G B, with sometimes a 4th Alpha channel on PNG
 arrs = [None, None, None]
 for i in range(3):
     #arr = np.copy(image[i][-4000:, -4000:])
     arr = np.copy(image[...,i])
+
+    # Normalization between 0 and 250
     mx = arr.max()
     arr = arr / (1.0 * mx) * 250
+
+    # Normalization using the average value of the channel
     av = arr.mean()
     print(av)
-    arr = arr * 150 / av
+    arr = arr * 100 / av
+
+    # Limiting the maximal value
     arr[arr > 250] = 250
+
     #print(arr.shape)
     arrs[i] = arr
 
@@ -31,3 +45,5 @@ rgbArray[..., 2] = arrs[2]
 print("saving")
 img = Image.fromarray(rgbArray)
 img.save('Paris_'+image_num+'/out_'+image_num+'.png')
+
+print("done")
