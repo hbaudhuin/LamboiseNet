f0d6b2fd0ad32b78c2e656f2b797f8fd051616fe
hbaudhuin <heloise.baudhuin@student.uclouvain.be>
2019-11-13 12:11:26 +0100

    Correction the Unet (addition of MaxPool in downscaling layers)

--- Models/basicUnet.py
+++ Models/basicUnet.py
@@ -13,8 +13,10 @@ class basicUnet(nn.Module):
         self.n_channels = n_channels
 
         self.input_layer = DoubleConvolutionLayer(n_channels, 64)
-        self.downscaling_layer1 = DoubleConvolutionLayer(64, 128)
-        self.downscaling_layer2 = DoubleConvolutionLayer(128, 256)
+        self.downscaling_layer1 = nn.Sequential(nn.MaxPool2d(2),
+                                                DoubleConvolutionLayer(64, 128))
+        self.downscaling_layer2 = nn.Sequential(nn.MaxPool2d(2),
+                                                DoubleConvolutionLayer(128, 256))
         # TODO add padding ?
         self.upscaling_layer1 = DoubleConvolutionLayer(256, 128)
         self.upscaling_layer = DoubleConvolutionLayer(128, 64)
@@ -22,20 +24,22 @@ class basicUnet(nn.Module):
 
     def forward(self, x):
         out0 = self.input_layer(x)
+
         out1 = self.downscaling_layer1(out0)
         out = self.downscaling_layer2(out1)
-        out= self.Upsample(out, scale_factor=2, mode='bilinear', align_corners=True)
-        out = cat([out, out1], dim = 1)
+
+        out = self.Upsample(out, scale_factor=2, mode='bilinear', align_corners=True)
+        out = cat([out, out1], dim=1)
         out = self.upscaling_layer1(out)
-        out= self.Upsample(out)
-        out = cat([out,x], dim =1 )
+        out = self.Upsample(out)
+        out = cat([out, x], dim=1)
         out = self.upscaling_layer2(out)
         output = self.output_layer(out)
         return output
 
 
 class DoubleConvolutionLayer(nn.Module):
-    def __init__(self, n_channels_input, n_channels_output) :
+    def __init__(self, n_channels_input, n_channels_output):
         self.double_layer = nn.Sequential(nn.Conv2d(n_channels_input, n_channels_output, kernels_size=3, padding=1),
                                           nn.BatchNorm2d(n_channels_output),
                                           nn.ReLu(inplace=True),
@@ -43,10 +47,6 @@ class DoubleConvolutionLayer(nn.Module):
                                           nn.BatchNorm2d(n_channels_output),
                                           nn.ReLU(inplace=True))
 
-
-
-    def forward(self, x) :
+    def forward(self, x):
         x = self.double_layer(x)
         return x
-
-
